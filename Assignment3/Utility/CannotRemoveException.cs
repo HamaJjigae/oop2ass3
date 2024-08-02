e860a1786b4bc248bf5f1f205d492388924700de
Matta <166758235+HamaJjigae@users.noreply.github.com>
2024-08-01 18:52:51 -0600

    Add files via upload
    2nd attempt at initial commit.

---
+++ Assignment3/Utility/CannotRemoveException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3.Utility
+{
+    public class CannotRemoveException : Exception
+    {
+        public CannotRemoveException() { }
+
+        public CannotRemoveException(string message) : base(message) { }
+
+        public CannotRemoveException(string message, Exception inner) : base(message, inner) { }
+    }
+}
---
+++ Assignment3/Utility/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Assignment3.Utility
+{
+    [DataContract]
+    internal class Node
+    {
+        [DataMember]
+        public User Value { get; set; }
+        [DataMember]
+        public Node Next { get; set; }
+
+        public Node(User value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+}
